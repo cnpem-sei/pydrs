fd396a8238edbdf8c0d52e280adb6146416a83e0
gfrn <guilherme.freitas@cnpem.br>
2022-08-18 11:06:18 -0300

    Removes unnecessary string formatting

--- src/pydrs/validation.py
+++ src/pydrs/validation.py
@@ -87,7 +87,7 @@ def validate(func):
 
 def print_deprecated(func):
     def wrapper(*args, **kwargs):
-        warn(f"From 2.0.0, most functions will not loop implicitly. Use a 'for' or 'while' loop instead")
+        warn("From 2.0.0, most functions will not loop implicitly. Use a 'for' or 'while' loop instead")
         return func(*args, **kwargs)
 
     return wrapper
